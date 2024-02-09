7c6bbc8865c462a8de960449c7176c5c75138073
rafaelgsousa <elderrafaelgomes@gmail.com>
2024-02-09 16:37:06 -0300

    refactor persons.urls

--- persons/urls.py
+++ persons/urls.py
@@ -1,7 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
 
 from .views import *
 
@@ -12,6 +10,4 @@ view_customeusers.register('', CustomUserView, basename='users-router')
 
 urlpatterns = [
     path('persons', include(view_customeusers.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
