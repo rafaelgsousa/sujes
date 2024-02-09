ff4c4bbb28b94d42817f07d3e4744d94c9fc25c9
rafaelgsousa <elderrafaelgomes@gmail.com>
2024-02-08 22:13:14 -0300

    feature: simple jwt for create tokens

--- project/urls.py
+++ project/urls.py
@@ -18,10 +18,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/persons', include('persons.urls'))
+    path('api/persons', include('persons.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
